92f4d83ff198bc08549075efc02a5b14f73e5810
Zaq Siefert <zsiefert@gmail.com>
2023-12-06 18:58:10 -0500

    added serializers, class views, urls, and imported drf

--- littlelemon/urls.py
+++ littlelemon/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from restaurant.views import BookingView
+
+DefaultRouter.register(r'tables', BookingView)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('restaurant/', include('restaurant.urls'))
+    path('restaurant/', include('restaurant.urls')),
+    path('restaurant/booking/', include(DefaultRouter.urls))
 ]
